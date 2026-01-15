ae5278b3d81cd93cd67b61629584021d010cdb0e
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-01-15 13:09:34 +0530

    updated

--- src/Trigonometry.java
+++ src/Trigonometry.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Trigonometry {
 
     // TODO: Create 'public static double sine(double angle)'
@@ -10,5 +11,25 @@ public class Trigonometry {
         // TODO: Read angle
         // TODO: Call sine() and cosine()
         // TODO: Print results in the required format
-    
+    public static double sine(double angle)
+    {
+        double rad = Math.toRadians(angle);
+        double sine = Math.sin(rad);
+        return sine;
+
+    }
+    public static double cosine(double angle)
+    {
+        double rad = Math.toRadians(angle);
+        double cos = Math.cos(rad);
+        return cos;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double angle = sc.nextDouble();
+        System.out.println("Sine: "+sine(angle));
+        System.out.println("Cosine: "+cosine(angle));
+        
+    }
 }
